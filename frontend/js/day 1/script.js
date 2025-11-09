6ac08e806f2912c70616802b4921270a5848bdcc
Ravikrp208 <ravikrp208@gmail.com>
2025-11-09 23:11:38 +0530

    day3

--- frontend/js/day 1/script.js
+++ frontend/js/day 1/script.js
@@ -1,3 +1,44 @@
-alert();
-let a = 10; 
-console .log('hello Ravi ');
+// alert();
+
+console.log("hello Ravi ");
+console.log(10 + 5);
+
+console.warn("something is worng");
+
+console.info("server started successfully!");
+
+let user = {
+  name: "Ravi",
+  email: "ravikrp@gamil.com",
+  course: "javascript",
+};
+console.table(user);
+
+console.log("User info");
+console.log("Name: Ravi kumar");
+console.log("Course: js");
+console.groupEnd();
+
+let name = "apple ,banana, grapes";
+console.log(`hello ,${name}!welcome to js`);
+
+let fruits = "apple, banana, grapes";
+console.log(fruits.split(",")); //["apple", " banana", " grapes"];
+
+let sentence = "I love HTML";
+console.log(sentence.replace("HTML","javaScript"));
+
+let text ="Learning javaScript is fun";
+console.log(text.includes("fun"));//true
+
+let a=10;
+let b=12;
+if (a>b){
+    console.log("a is greater than b");
+}
+else{
+    console.log("b is greater than a")
+}
+3 + 5;
+"hello java" +"world";
+
--- frontend/js/day 2/script.js
+++ frontend/js/day 2/script.js
@@ -1,1 +1,12 @@
-console.warn("something is worng");
+// console.erro("this is a warning!");
+console.time("loopTime");
+for (let i = 0; i < 100000; i++) {}
+console.timeEnd("loopTime");
+
+// console.clear();
+let name = "Ravi kumar";
+console.log(name);
+
+let str ="javaScript";
+console.log(str.slice(0,4));//java
+ 
