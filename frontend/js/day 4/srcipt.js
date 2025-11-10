d346a1c143dc632c42dfb0b8c120e57676db8b3f
Ravikrp208 <ravikrp208@gmail.com>
2025-11-10 22:57:09 +0530

    day 4

---
+++ frontend/js/day 4/srcipt.js
@@ -0,0 +1,13 @@
+function greeting (guest ="guest"){
+    console.log(`hii ${guest}`);
+}
+greeting("");
+
+let obj ={
+    name:"Ravi",
+    email:"ravikumar@gmail.com"
+
+};
+for(let id in obj){
+    console.log(obj.id)
+};
