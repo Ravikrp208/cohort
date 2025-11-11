ee774223ebcac92da4c3a77781222cca49f5193e
Ravikrp208 <ravikrp208@gmail.com>
2025-11-11 23:30:48 +0530

    function

--- frontend/js/day 4/srcipt.js
+++ frontend/js/day 4/srcipt.js
@@ -1,7 +1,7 @@
 function greeting (guest ="guest"){
     console.log(`hii ${guest}`);
 }
-greeting("");
+greeting("ravi");
 
 let obj ={
     name:"Ravi",
@@ -11,3 +11,39 @@ let obj ={
 for(let id in obj){
     console.log(obj.id)
 };
+
+ function add(a,b){
+    return a+b;
+
+}
+let ans = (15+6);
+console.log(ans);
+
+// part 1
+function addUnlimited(...nums){
+let sum = 0;
+nums.forEach(function (val){
+    sum = sum + val;
+});
+console.log(sum);
+}
+addUnlimited(1,3,4,5)
+
+//  part 2
+// function addUnlimited(...nums) {
+//   let ans = nums.reduce(function (acc, val){
+//     return acc +val ;
+//   },0);
+//   console.log(ans);
+// }
+// addUnlimited(1, 5, 4, 5);
+
+
+// part 3
+// function addUnlimited(...nums) {
+//   let ans = nums.reduce(function (acc, val) {
+//     return acc * val;
+//   }, 0);
+//   console.log(ans);
+// }
+// addUnlimited(1,4, 5);
