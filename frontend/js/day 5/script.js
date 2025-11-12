4bee1a32381254ec982ccab749a0d7ab467e3cf6
Ravikrp208 <ravikrp208@gmail.com>
2025-11-12 21:08:44 +0530

    day 5

---
+++ frontend/js/day 5/script.js
@@ -0,0 +1,17 @@
+function abcd(A,B){
+
+    console.log(A+B);
+}
+abcd(1,2)
+abcd(1,2)
+abcd(1, 2);
+abcd(1,2)
+
+let global =0;
+function inpure(a) {
+  console.log(a);
+}
+ global++;
+// console.log(a + global);
+inpure(2);
+inpure(2);
