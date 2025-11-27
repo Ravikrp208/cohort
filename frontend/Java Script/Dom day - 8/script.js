a30606ee8eb05afb65200788a8211c438fb8cdb8
Ravikrp208 <ravikrp208@gmail.com>
2025-11-27 23:43:43 +0530

    Project

--- frontend/Java Script/Dom day - 8/script.js
+++ frontend/Java Script/Dom day - 8/script.js
@@ -4,10 +4,14 @@
 // allh1.forEach(function(){
 //     console.log('hello ravi');
 
-   
-    
-
 // })
 
-let outer =document.querySelector('#outer');
-outer.childNodes(outer.childNodes);
+// let outer =document.querySelector('#outer');
+// outer.childNodes(outer.childNodes);
+
+
+// let names="ravi, rani,kajal, sima"
+// console.log(names.split(","))
+
+
+
