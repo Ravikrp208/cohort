9f1c1b378361d9826f9a70e65e2795617eaf01f0
Ravikrp208 <ravikrp208@gmail.com>
2025-12-01 23:47:48 +0530

    class

--- frontend/JAVA SCRIPT OOP/Day 1/script.js
+++ frontend/JAVA SCRIPT OOP/Day 1/script.js
@@ -1,14 +1,26 @@
-class mango{
-    constructor(){
-        this.prince=69;
-        this.state="jharkhand";
-        this.juse="mangojus"
-
-    }
-    
+class mango {
+  constructor() {
+    this.prince = 69;
+    this.state = "jharkhand";
+    this.juse = "mangojus";
+  }
 }
- let ravi = new mango();
- let ravi2 = new mango();
- let ravi3 = new mango(); 
- let ravi4 = new mango(); 
- let ravi5 = new mango();
+let ravi = new mango();
+let ravi2 = new mango();
+let ravi3 = new mango();
+let ravi4 = new mango();
+let ravi5 = new mango();
+
+class book {
+  constructor(name,prince,author,color) {
+    this.name = name ;  //java script";
+    this.price =  prince ;     //500;
+    this.author = author ;     //"Ravikrp";
+    this.color =   color;        //"yellow";
+  }
+  pannaPalto() {}
+  bookMark() {}
+  padho() {}
+}
+let k= new book("java",590, "Rani","raegreen");
+let k2 = new book();
