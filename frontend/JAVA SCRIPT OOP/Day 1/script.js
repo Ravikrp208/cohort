5d6b9c4e800aec33768ba7c4cc76e4078ec39555
Ravikrp208 <ravikrp208@gmail.com>
2025-12-01 23:33:53 +0530

    class

---
+++ frontend/JAVA SCRIPT OOP/Day 1/script.js
@@ -0,0 +1,14 @@
+class mango{
+    constructor(){
+        this.prince=69;
+        this.state="jharkhand";
+        this.juse="mangojus"
+
+    }
+    
+}
+ let ravi = new mango();
+ let ravi2 = new mango();
+ let ravi3 = new mango(); 
+ let ravi4 = new mango(); 
+ let ravi5 = new mango();
