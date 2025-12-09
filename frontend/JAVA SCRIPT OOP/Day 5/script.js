60a05785ed6ba161f360675a92e657d6e2a09e8c
Ravikrp208 <ravikrp208@gmail.com>
2025-12-09 22:52:50 +0530

    promise

--- frontend/JAVA SCRIPT OOP/Day 5/script.js
+++ frontend/JAVA SCRIPT OOP/Day 5/script.js
@@ -1,15 +1,23 @@
-function getUsr (username ,cd){
-    setTimeout 
-}
-async function getData() {
-  let result = await fetch("https://example.com");
-  console.log(result);
-}
-
-function fetchUser() {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve("User loaded"), 1000);
+fetch("https://randomuser.me/api/")
+  .then((raw) => raw.json())
+  .then((data) => {
+    console.log(data); 
+    console.log(data.results[0].name.first); 
+  })
+  .catch((err) => {
+    console.log(err);
   });
-}
 
-fetchUser().then((result) => console.log(result));
+
+let h1 = document.querySelector('h1');
+h1.addEventListener("click" ,function(){
+  console.log(this.innerHTML)
+  this.style.backgroundColor="yellow";
+});
+
+let btn = document.querySelector ('button');
+btn.addEventListener("click",function (){
+  console.log("button was click")
+})
+
+console.info("i am a software developer")
