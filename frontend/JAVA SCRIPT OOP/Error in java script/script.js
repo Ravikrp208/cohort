54a23dda79d9a19dc3b8e594bbe839d7ba0aec1a
Ravikrp208 <ravikrp208@gmail.com>
2025-12-10 21:35:58 +0530

    error

--- frontend/JAVA SCRIPT OOP/Error in java script/script.js
+++ frontend/JAVA SCRIPT OOP/Error in java script/script.js
@@ -1,1 +1,14 @@
 // there are many type of error handling in java Script
+try {
+    let a=12;
+    console.log(a);
+
+}
+catch (error){
+    console.log(erroe);
+}
+finally
+{
+  console.log("Ravi kumar pandit")
+
+}
