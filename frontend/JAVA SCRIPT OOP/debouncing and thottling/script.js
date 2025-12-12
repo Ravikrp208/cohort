6156669d9d80ebd9391d19604c623506632d23a2
Ravikrp208 <ravikrp208@gmail.com>
2025-12-12 23:25:23 +0530

    callback

--- frontend/JAVA SCRIPT OOP/debouncing and thottling/script.js
+++ frontend/JAVA SCRIPT OOP/debouncing and thottling/script.js
@@ -1,2 +1,33 @@
-// document.querySelector(#search);
+function debounce(fun, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fun.apply(this, args);
+    }, delay);
+  };
+}
+
+document.querySelector("#search").addEventListener(
+  "input",
+  debounce(function () {
+    console.log("User stopped typing...");
+  }, 500)
+); 
+
+//what is json parse
+/*let jsonString = '{"name":"Ravi","age":25}';
+ let obj = JSON.parse(jsonString);
+
+console.log(obj.name);*/ 
+// Ravi
+
+//what is stringify
+let obj = { name: "Ravi", age: 25 };
+let jsonString = JSON.stringify(obj);
+
+console.log(jsonString);
+
+// {"name":"Ravi","age":25}
+
 
