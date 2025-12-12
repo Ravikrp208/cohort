c009a7d58ab37c2cbb55084b6b437446693f2718
Ravikrp208 <ravikrp208@gmail.com>
2025-12-12 21:55:15 +0530

    callback

--- frontend/JAVA SCRIPT OOP/Day 3/script.js
+++ frontend/JAVA SCRIPT OOP/Day 3/script.js
@@ -1,1 +1,25 @@
 // function in java script //
+
+
+// Question call back hell //
+
+function abc (fn)
+{
+    fn( function (fn3){
+        fn3(function (fn5){
+            fn5();
+        });
+
+    });
+}
+abc (function ( fun2){
+    fun2(function (fn4){
+        fn4(function (){
+
+            console.log ("Ravi kumar pandit");
+        });
+    });
+});
+
+
+// call back //
