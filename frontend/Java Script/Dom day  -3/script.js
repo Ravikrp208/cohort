945289046a988335aeca5753dc579790349a6741
Ravikrp208 <ravikrp208@gmail.com>
2025-12-16 23:53:15 +0530

    operator

---
+++ frontend/Java Script/Dom day  -3/script.js
@@ -0,0 +1,26 @@
+let a = 10;
+let b = 3;
+
+console.log(a + b); // Addition → 13
+console.log(a - b); // Subtraction → 7
+console.log(a * b); // Multiplication → 30
+console.log(a / b); // Division → 3.33
+console.log(a % b); // Modulus → 1
+console.log(a ** b); // Exponent → 1000
+
+
+// 2nd Question
+
+let x = true;
+let y = false;
+
+console.log(x && y); // AND → false
+console.log(x || y); // OR → true
+console.log(!x);     // NOT → false
+
+// 3nd Question 
+let firstName = "Ravi";
+let middleName ="Kumar"
+let lastName = "Pandit";
+
+console.log(firstName + " " + middleName +" "+ lastName); // John Doe
