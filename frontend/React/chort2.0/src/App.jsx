027225d3f99ee4d89a5aa8289ac0e590a4ff2e2b
Ravikrp208 <ravikrp208@gmail.com>
2025-12-18 23:37:47 +0530

    1st day to React

---
+++ frontend/React/chort2.0/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2>hello guys , i am Ravi kumar Pandit</h2>
+      <h2>cohort 2.0 is very good batch </h2>
+      </div>
+  )
+}
+
+export default App
