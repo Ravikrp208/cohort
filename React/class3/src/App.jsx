55ccc3ad3d33dd48ccba8f88fd1e5a0ac1779d4b
Ravikrp208 <ravikrp208@gmail.com>
2026-01-08 23:54:46 +0530

    motion

--- React/class3/src/App.jsx
+++ React/class3/src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { use } from "react";
 import axios from "axios";
+import Home from "./components/Home";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
 
 const App = () => {
+  // gsap.registerPlugin(useGSAP)
+  // useGSAP (()=>{
+  //   gsap.to('html',{
+  //     backgroundColor:"red",
+  //     scrollTrigger:{
+  //       trigger:"#section-1",
+  //     }
+  //   })
+  // },[])
+
   const getData = async () => {
     try {
       const res = await axios.get("https://fakestoreapi.com/products");
@@ -13,7 +26,13 @@ const App = () => {
 
   return (
     <div>
-      <button onClick={getData}>Data</button>
+      <main>
+        <section id ="section 1">2</section>
+        <section id ="section 2" >1</section>
+        <section id ="section 3">3</section>
+        <section id ="section 4">4</section>
+      </main>
+      {/* <Home/> */}
     </div>
   );
 };
